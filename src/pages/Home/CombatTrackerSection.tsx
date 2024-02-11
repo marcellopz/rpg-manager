0b4b4bde41d3b16c4bb13a0b5f281626e3b240ee
ZanGit <marcellopz@hotmail.com>
2024-02-10 22:28:28 -0300

    combat home motion

--- src/pages/Home/CombatTrackerSection.tsx
+++ src/pages/Home/CombatTrackerSection.tsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const CombatTrackerSection: React.FC = () => {
   return (
-    <section className="combat-tracker-section">
-      <h2>Track the combat stats</h2>
-      <div className="combat-video">EM BREVE</div>
-    </section>
+    <motion.section
+      initial={{ opacity: 0, y: 50 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.5 }}
+    >
+      <div className="combat-tracker-section">
+        <h2>Track the combat stats</h2>
+        <div className="combat-video">EM BREVE</div>
+      </div>
+    </motion.section>
   );
 };
 
