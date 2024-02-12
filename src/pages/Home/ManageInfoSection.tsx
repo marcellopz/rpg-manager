2b6efd76c950e61349cd31bf0d006f3a751e5cba
ZanGit <marcellopz@hotmail.com>
2024-02-12 16:06:13 -0300

    correct icons and text

--- src/pages/Home/ManageInfoSection.tsx
+++ src/pages/Home/ManageInfoSection.tsx
@@ -6,7 +6,7 @@ const ManageInfoItem = ({ title, text, icon }: any) => {
       <div className="manage-info-item-icon">
         <img
           src={icon}
-          width={70}
+          height={70}
         />
       </div>
       <div className="manage-info-item-text">
@@ -28,42 +28,46 @@ const ManageInfoSection = () => {
     >
       <div className="manage-info-content">
         <div className="left-text">
-          <h2>Manage your campaigns</h2>
+          <h2>Master Your Campaign</h2>
           <p>
-            RPG Manager allows you to create and manage your campaigns. You can
-            create new campaigns, edit existing ones, and delete them.
+            Streamline your adventure with our comprehensive tools designed for
+            table RPG enthusiasts. Organize your campaign information
+            effortlessly with customizable categories, manage character sheets,
+            and collaborate seamlessly with your party. Our platform ensures
+            that every detail of your journey is just a click away, enabling you
+            and your fellow adventurers to focus on the story ahead.
           </p>
         </div>
         <div className="items-right">
           <ManageInfoItem
-            title="Create Campaigns"
-            text="Create new campaigns and manage them."
-            icon="/assets/weight.png"
+            title="Categories & Subcategories"
+            text="Organize your campaign's lore, quests, and NPCs with ease. Use categories and subcategories to keep your information structured and accessible."
+            icon="/assets/home/diagram.png"
           />
           <ManageInfoItem
-            title="Manage Characters"
-            text="Create and manage characters for your campaigns."
-            icon="/assets/weight.png"
+            title="Character Sheet Management"
+            text="Keep your character's journey at your fingertips. Store and update character sheets with all the crucial stats and backstory details."
+            icon="/assets/home/file.png"
           />
           <ManageInfoItem
-            title="Manage Items"
-            text="Create and manage items for your campaigns."
-            icon="/assets/weight.png"
+            title="Rich Text Editing"
+            text="Bring your campaign to life with rich text features. Highlight important details, use bullet points for quests, and italicize magical items."
+            icon="/assets/home/text-formatting.png"
           />
           <ManageInfoItem
-            title="Another Item"
-            text="Description of another item."
-            icon="/assets/weight.png"
+            title="Player Invitations"
+            text="Expand your adventure by inviting players directly via email. Seamless integration allows for quick access to the campaign for all invited members."
+            icon="/assets/home/email.png"
           />
           <ManageInfoItem
-            title="Another Item"
-            text="Description of another item."
-            icon="/assets/weight.png"
+            title="Public & Private Sections"
+            text="Share information with your party through public categories, or keep personal notes and strategies hidden in your private section."
+            icon="/assets/home/visibility.png"
           />
           <ManageInfoItem
-            title="Another Item"
-            text="Description of another item."
-            icon="/assets/weight.png"
+            title="Collaboration Made Easy"
+            text="Collaborate in real-time with your party. Share updates, plan your next move, and navigate your campaign together."
+            icon="/assets/home/partnership.png"
           />
         </div>
       </div>
